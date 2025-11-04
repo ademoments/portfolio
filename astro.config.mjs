2022c0a3c849a5399279e7989af8978a5e5e8fdc
Adem Bounaidja-Rachedi <adem75bounaidja@gmail.com>
2025-11-04 21:34:03 +0100

    chore: config Astro for GitHub Pages (/portfolio)

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,16 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
+import { fileURLToPath } from 'node:url';
 
 export default defineConfig({
-  site: 'https://ademoments.github.io/portfolio',
+  site: 'https://ademoments.github.io', 
+  base: '/portfolio',                    
   integrations: [react(), tailwind({ applyBaseStyles: false }), sitemap()],
   prefetch: true,
+  vite: {
+    resolve: {
+      alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) },
+    },
+  },
 });
